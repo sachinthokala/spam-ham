1f3cf0031b328728f61eaa23e28c1c23dcac67d2
sachinthokala <97581273+sachinthokala@users.noreply.github.com>
2022-03-23 12:25:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+# the above command is a magic command to create a python file from colab
+#it takes the entire code and puts it in app.py 
+#since streamlit runs only on CLI(command line interface) using .py files
+import streamlit as st
+st.title("MY FIRST STREAMLIT WEB APP")
+st.subheader("Welcome to AI tutorial")
+st.write("RNSIT")
